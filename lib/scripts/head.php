50dddec915a637b029bbabcf0b8e1a81a9ac2423
assydata3 <assydata.ipad03@gmail.com>
2024-02-19 15:26:38 +0700

    start

---
+++ lib/scripts/head.php
@@ -0,0 +1,25 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="description" content="Admindek Bootstrap admin template made using Bootstrap 4 and it has huge amount of ready made feature, UI components, pages which completely fulfills any dashboard needs." />
+<meta name="keywords" content="flat ui, admin Admin , Responsive, Landing, Bootstrap, App, Template, Mobile, iOS, Android, apple, creative app">
+<meta name="author" content="colorlib" />
+
+<?php /*?>
+<link rel="icon" href="https://colorlib.com/polygon/admindek/files/assets/images/favicon.ico" type="image/x-icon">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Quicksand:500,700" rel="stylesheet">
+<?php */?>
+
+
+<link href="source/fonts/openSan.css" rel="stylesheet">
+<link href="source/fonts/quickSan.css" rel="stylesheet">
+
+
+<link rel="stylesheet" type="text/css" href="source/css/bootstrap.min.css">
+<link rel="stylesheet" href="source/css/waves.min.css" type="text/css" media="all">
+<link rel="stylesheet" type="text/css" href="source/css/feather.css">
+<link rel="stylesheet" type="text/css" href="source/css/font-awesome-n.min.css">
+<link rel="stylesheet" href="source/css/chartist.css" type="text/css" media="all">
+<link rel="stylesheet" type="text/css" href="source/css/style.css">
+<link rel="stylesheet" type="text/css" href="source/css/widget.css">
---
+++ lib/scripts/javascript_origin.php
@@ -0,0 +1,35 @@
+
+<script data-cfasync="false" src="source/js/email-decode.min.js"></script><script type="d2d1d6e2f87cbebdf4013b26-text/javascript" src="source/js/jquery.min.js"></script>
+<script type="d2d1d6e2f87cbebdf4013b26-text/javascript" src="source/js/jquery-ui.min.js"></script>
+<script type="d2d1d6e2f87cbebdf4013b26-text/javascript" src="source/js/popper.min.js"></script>
+<script type="d2d1d6e2f87cbebdf4013b26-text/javascript" src="source/js/bootstrap.min.js"></script>
+
+<script src="source/js/waves.min.js" type="d2d1d6e2f87cbebdf4013b26-text/javascript"></script>
+
+<script type="d2d1d6e2f87cbebdf4013b26-text/javascript" src="source/js/jquery.slimscroll.js"></script>
+
+<script src="source/js/jquery.flot.js" type="d2d1d6e2f87cbebdf4013b26-text/javascript"></script>
+<script src="source/js/jquery.flot.categories.js" type="d2d1d6e2f87cbebdf4013b26-text/javascript"></script>
+<script src="source/js/curvedlines.js" type="d2d1d6e2f87cbebdf4013b26-text/javascript"></script>
+<script src="source/js/jquery.flot.tooltip.min.js" type="d2d1d6e2f87cbebdf4013b26-text/javascript"></script>
+
+<script src="source/js/chartist.js" type="d2d1d6e2f87cbebdf4013b26-text/javascript"></script>
+
+<script src="source/js/amcharts.js" type="d2d1d6e2f87cbebdf4013b26-text/javascript"></script>
+<script src="source/js/serial.js" type="d2d1d6e2f87cbebdf4013b26-text/javascript"></script>
+<script src="source/js/light.js" type="d2d1d6e2f87cbebdf4013b26-text/javascript"></script>
+
+<script src="source/js/pcoded.min.js" type="d2d1d6e2f87cbebdf4013b26-text/javascript"></script>
+<script src="source/js/vertical-layout.min.js" type="d2d1d6e2f87cbebdf4013b26-text/javascript"></script>
+<script type="d2d1d6e2f87cbebdf4013b26-text/javascript" src="source/js/custom-dashboard.min.js"></script>
+<script type="d2d1d6e2f87cbebdf4013b26-text/javascript" src="source/js/script.min.js"></script>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13" type="d2d1d6e2f87cbebdf4013b26-text/javascript"></script>
+<script type="d2d1d6e2f87cbebdf4013b26-text/javascript">
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-23581568-13');
+</script>
+<script src="source/js/rocket-loader.min.js" data-cf-settings="d2d1d6e2f87cbebdf4013b26-|49" defer=""></script>
